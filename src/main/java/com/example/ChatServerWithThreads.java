5a0b7cb299ff59b0eaf96da14a987397f05ffa5c
TheLegendaryPotato-dev <yunacho914@gmail.com>
2026-03-30 20:22:01 +0000

    finishJFrameHardToDoInAnotherCountryBecauseParentsHaveTooManyPlansIsFiveInTheMorningRightNow

--- src/main/java/com/example/ChatServerWithThreads.java
+++ src/main/java/com/example/ChatServerWithThreads.java
@@ -2,16 +2,17 @@ package com.example;
 
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * This program is a server that takes connection requests on
  * the port specified by the constant LISTENING_PORT.  When a
- * connection is opened, the program should allow the client to send it messages. The messages should then 
+ * connection is opened, the program should allow the client to send it messages. The messages should then
  * become visible to all other clients.  The program will continue to receive
  * and process connections until it is killed (by a CONTROL-C,
- * for example). 
- * 
+ * for example).
+ *
  * This version of the program creates a new thread for
  * every connection request.
  */
@@ -30,7 +31,11 @@ public class ChatServerWithThreads {
             listener = new ServerSocket(LISTENING_PORT);
             System.out.println("Listening on port " + LISTENING_PORT);
             while (true) {
-                  // Accept next connection request and handle it.
+                connection = listener.accept();
+                // Accept next connection request and handle it.
+                ConnectionHandler h = new ConnectionHandler(connection);
+                h.start();
+                 
             }
         }
         catch (Exception e) {
@@ -47,20 +52,50 @@ public class ChatServerWithThreads {
      *  client.
      */
     private static class ConnectionHandler extends Thread {
+        private static volatile ArrayList<ConnectionHandler> handlers; //This is shared among all the connection handlers
         Socket client;
+        ObjectOutputStream talk; //you'll need to define this one for when you're ready to talk back to the client!
+        ObjectInputStream listen;
         ConnectionHandler(Socket socket) {
             client = socket;
+            if(handlers== null){
+                handlers = new ArrayList();
+            }
+            handlers.add(this);
+            try{
+            listen = new ObjectInputStream(client.getInputStream());
+            talk = new ObjectOutputStream(client.getOutputStream());
+            }
+            catch(Exception e){}
+           
         }
         public void run() {
             String clientAddress = client.getInetAddress().toString();
             while(true) {
-	            try {
-	            	//your code to send messages goes here.
-	            }
-	            catch (Exception e){
-	                System.out.println("Error on connection with: " 
-	                        + clientAddress + ": " + e);
-	            }
+                try {
+                    String message = (String)listen.readObject();
+                    if(!message.equals("disconnect")){
+                        for(int i = 0; i< handlers.size(); i++){
+                            //System.out.println(message);
+                            handlers.get(i).talk.writeObject(message);
+                            handlers.get(i).talk.flush();
+                        }
+                    }
+                    else{
+                        System.out.println("closing connection");
+                        break;
+                       
+                    }
+                }
+                catch(EOFException e){
+                    System.out.println("the client disconnected, bye!!!");
+                    handlers.remove(this);
+                    break;
+                }
+                catch (Exception e){
+                    System.out.println("Error on connection with: "
+                            + clientAddress + ": " + e);
+                }
             }
         }
     }
--- src/main/java/com/example/SocketClientExample.java
+++ src/main/java/com/example/SocketClientExample.java
@@ -1,47 +1,78 @@
 package com.example;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+import javax.swing.*;
 
 
 public class SocketClientExample {
-	
-	
-	/*
-	 * Modify this example so that it opens a dialogue window using java swing, 
-	 * takes in a user message and sends it
-	 * to the server. The server should output the message back to all connected clients
-	 * (you should see your own message pop up in your client as well when you send it!).
-	 *  We will build on this project in the future to make a full fledged server based game,
-	 *  so make sure you can read your code later! Use good programming practices.
-	 *  ****HINT**** you may wish to have a thread be in charge of sending information 
-	 *  and another thread in charge of receiving information.
-	*/
+   
+   
+    /*
+     * Modify this example so that it opens a dialogue window using java swing,
+     * takes in a user message and sends it
+     * to the server. The server should output the message back to all connected clients
+     * (you should see your own message pop up in your client as well when you send it!).
+     *  We will build on this project in the future to make a full fledged server based game,
+     *  so make sure you can read your code later! Use good programming practices.
+     *  ****HINT**** you may wish to have a thread be in charge of sending information
+     *  and another thread in charge of receiving information.
+    */
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
         //get the localhost IP address, if server is running on some other IP, you need to use that
         InetAddress host = InetAddress.getLocalHost();
-        Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        for(int i=0; i<5;i++){
-            //establish socket connection to server
-            socket = new Socket(host.getHostName(), 9876);
+        Socket socket = new Socket(host.getHostName(), 9876);
             //write to socket using ObjectOutputStream
-            oos = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("Sending request to Socket Server");
-            if(i==4)oos.writeObject("exit");
-            else oos.writeObject(""+i);
-            //read the server response message
-            ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Message: " + message);
-            //close resources
-            ois.close();
-            oos.close();
-            Thread.sleep(100);
-        }
+        ObjectOutputStream   oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        JFrame chatBoot = new JFrame();
+        JTextField input = new JTextField();
+        JTextArea otherTexters = new JTextArea();
+        chatBoot.add(input);
+        chatBoot.add(otherTexters);
+
+        input.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                try{
+                    oos.writeObject(input.getText());
+                    oos.flush();
+                }catch(Exception x){}
+
+            }});
+
+            while(true){
+                try {
+                    String message = (String)ois.readObject();
+                    //add text
+                   
+                }
+                catch(EOFException e){
+                    System.out.println("the server disconnected, bye!!!");
+                   
+                }
+                catch (Exception e){
+                    System.out.println("Error on connection with: "
+                            + clientAddress + ": " + e);
+            }
+
+        // while(!(line = input.nextLine()).equals("disconnect")){
+        //     oos.writeObject(line);
+        //     oos.flush();       
+        //     
+        // }
+        // ObjectInputStream listen;
+        // socket.shutdownOutput();
+        
+        // System.out.println("connection closed!");
     }
 }
